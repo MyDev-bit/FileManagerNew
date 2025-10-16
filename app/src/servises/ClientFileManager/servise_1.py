7413b7cdeb46f332f76d5212ed43e71daccb4b71
MyDev-bit <ucomp5451@gmail.com>
2025-10-16 22:24:28 +0500

    Update servise_1.py

--- app/src/servises/ClientFileManager/servise_1.py
+++ app/src/servises/ClientFileManager/servise_1.py
@@ -23,7 +23,7 @@ class ClientFileManager:
             with file_patch.open("wb") as fl:
                     fl.write(upload.file.read())
 
-                    stmt = insert(Files).values({"file_id": f"{uuid.uuid4()/+9}", "file_name": file_name,
+                    stmt = insert(Files).values({"file_id": f"{uuid.uuid4()}", "file_name": file_name,
                                                  "file_extension":str(file_extension[1]),"user_load":user_nick})
                     db.execute(stmt)
                     db.commit()
